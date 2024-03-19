1997673f7131608347c0c5a15a9401b5aaa83ffd
zenmodeman <superzenmodeman@gmail.com>
2024-03-19 14:48:30 -0600

    Add Throat Chop TM

--- include/constants/tms_hms.h
+++ include/constants/tms_hms.h
@@ -51,7 +51,8 @@
     F(STEEL_WING) \
     F(SKILL_SWAP) \
     F(SNATCH) \
-    F(OVERHEAT)
+    F(OVERHEAT)\
+    F(THROAT_CHOP)
 
 #define FOREACH_HM(F) \
     F(CUT) \
--- src/data/items.h
+++ src/data/items.h
@@ -9975,16 +9975,18 @@ const struct Item gItemsInfo[] =
         .secondaryId = MOVE_OVERHEAT,
     },
 
-    [ITEM_TM51] =
+    [ITEM_TM_THROAT_CHOP] =
     {
         .name = _("TM51"),
         .price = 3000,
-        .description = sQuestionMarksDesc, // Todo
+        .description = COMPOUND_STRING(
+            "Chops the throat to disable\n"
+            "sound moves for a while."), 
         .importance = I_REUSABLE_TMS,
         .pocket = POCKET_TM_HM,
         .type = ITEM_USE_PARTY_MENU,
         .fieldUseFunc = ItemUseOutOfBattle_TMHM,
-        .secondaryId = MOVE_NONE, // Todo
+        .secondaryId = MOVE_THROAT_CHOP, 
     },
 
     [ITEM_TM52] =
