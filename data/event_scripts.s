c8e97f7aea631088072ec703a39b916c35d7c52a
zenmodeman <123277820+zenmodeman@users.noreply.github.com>
2024-03-31 23:21:14 -0600

    Initial Center Tutor setup

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -656,6 +656,7 @@ EventScript_SetBrineyLocation_Route109::
 	.include "data/scripts/obtain_item.inc"
 	.include "data/scripts/record_mix.inc"
 	.include "data/scripts/pc.inc"
+	.include "data/scripts/pkmn_center_tutor.inc"
 
 @ scripts/notices.inc? signs.inc? See comment about text/notices.inc
 Common_EventScript_ShowPokemartSign::
--- include/constants/vars.h
+++ include/constants/vars.h
@@ -156,13 +156,13 @@
 #define VAR_CONTEST_TYPE                                 0x4088
 #define VAR_SECRET_BASE_INITIALIZED                      0x4089
 #define VAR_CONTEST_PRIZE_PICKUP                         0x408A
-#define VAR_MONOTYPE                                     0x408B // For monotype settings, formerly unused
+#define VAR_MONOTYPE                                     0x408B // For monotype settings, formerly un used
 #define VAR_LITTLEROOT_HOUSES_STATE_BRENDAN              0x408C
 #define VAR_LITTLEROOT_RIVAL_STATE                       0x408D
 #define VAR_BOARD_BRINEY_BOAT_STATE                      0x408E
 #define VAR_DEVON_CORP_3F_STATE                          0x408F
 #define VAR_BRINEY_HOUSE_STATE                           0x4090
-#define VAR_UNUSED_0x4091                                0x4091 // Unused Var
+#define VAR_REMAINING_TUTOR                              0x4091 // Number of remaining Tutor moves in Resource mode
 #define VAR_LITTLEROOT_INTRO_STATE                       0x4092
 #define VAR_MAUVILLE_GYM_STATE                           0x4093
 #define VAR_LILYCOVE_MUSEUM_2F_STATE                     0x4094
