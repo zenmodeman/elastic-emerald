0bdb2636d6e0bd57ba9e0318b47e5ffc55110d21
zenmodeman <123277820+zenmodeman@users.noreply.github.com>
2024-12-01 21:21:23 -0700

    Remanent of additional changes necessary to build after the merge

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -193,6 +193,8 @@ static EWRAM_DATA struct PokemonSummaryScreenData
 EWRAM_DATA u8 gLastViewedMonIndex = 0;
 static EWRAM_DATA u8 sMoveSlotToReplace = 0;
 ALIGNED(4) static EWRAM_DATA u8 sAnimDelayTaskId = 0;
+EWRAM_DATA MainCallback gInitialSummaryScreenCallback = NULL; // stores callback from the first time the screen is opened from the party or PC menu
+
 
 // forward declarations
 static bool8 LoadGraphics(void);
